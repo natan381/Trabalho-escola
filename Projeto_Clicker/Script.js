c418337953d7f12af1ed8f40eebeefe6351a48fe
natan381 <ntnfellype@gmail.com>
2026-08-05 18:41:05 -0300

    ATUALIZAÇÃO V6
    -Fundo;
    -AutoClick;
    -Botão de Compra AutoClick;
    -Contador AutoClick;
    -Remoção Fundo do Botão Cookie.

--- Projeto_Clicker/Script.js
+++ Projeto_Clicker/Script.js
@@ -4,10 +4,34 @@ let x = 1; //Valor de biscoitos por click
 
 let preco = 10; //preço do primeiro Upgrade
 
-let preço2 = 200 //preço do segundo Upgrade
+let preço2 = 200; //preço do segundo Upgrade
 
+let preço3 = 1000; //preço do terceiro Upgrade
 
+let t = 0; //valor de biscoitos por segundo
 
+function autoClick() {
+    if (contador >= preço3) {
+        contador -= preço3;
+        t += 1;
+        preço3 *= 2;
+        document.getElementById("contador").innerHTML = contador;
+        document.getElementById("preço3").innerHTML = preço3;
+        document.getElementById("t").innerHTML = t;
+        setInterval(() => {
+        
+        contador += t;
+        
+        document.getElementById("contador").innerHTML = contador;
+        document.getElementById("t").innerHTML = t;
+    }, 1500);
+}else{
+    alert("Você não tem saldo suficiente para comprar o upgrade!");
+}
+
+    }
+
+    
 
 
 //Função de contador para adicionar cookies por click
@@ -43,7 +67,7 @@ function upgrade2 () {
     if (contador >= preço2) {
         contador -= preço2;
         x *= 2;
-        preço2 *= 4;
+        preço2 *= 3;
         document.getElementById("contador").innerHTML = contador;
         document.getElementById("preço2").innerHTML = preço2;
         document.getElementById("x").innerHTML = x;
